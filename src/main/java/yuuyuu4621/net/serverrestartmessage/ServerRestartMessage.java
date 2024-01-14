118ebefe7b1faa1ca3071855e9353ec83668ea07
Yuuyuu4621 <yuuyuu4621@gmail.com>
2024-01-14 21:35:16 +0900

    Flyコマンドを実装

--- src/main/java/yuuyuu4621/net/serverrestartmessage/ServerRestartMessage.java
+++ src/main/java/yuuyuu4621/net/serverrestartmessage/ServerRestartMessage.java
@@ -1,6 +1,7 @@
 package yuuyuu4621.net.serverrestartmessage;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import yuuyuu4621.net.serverrestartmessage.commands.countdown;
 import yuuyuu4621.net.serverrestartmessage.commands.restartmessage;
 
 public final class ServerRestartMessage extends JavaPlugin {
@@ -10,10 +11,10 @@ public final class ServerRestartMessage extends JavaPlugin {
     {
 
         getCommand("restartmessage").setExecutor(new restartmessage());
+        getCommand("countdown").setExecutor(new countdown());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
